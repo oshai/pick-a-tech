fe243da74407b80c8215f7c46130a191cbffe491
oshai <ohadshai@gmail.com>
2015-04-29 00:08:00 +0300

    added new pick page

--- pick-a-tech.js
+++ pick-a-tech.js
@@ -9,10 +9,15 @@ Picks = new Mongo.Collection("picks");
 Router.configure({
     layoutTemplate: 'ApplicationLayout'
 });
-Router.route('/', function() {
+Router.route('/', function () {
+	  this.redirect('/picks');
+	});
+Router.route('/picks', function() {
     this.render('pickListTemplate');
 });
-
+Router.route('/picks/new', function() {
+    this.render('newPickTemplate');
+});
 
 //Client Code
 if (Meteor.isClient) {
@@ -21,6 +26,25 @@ if (Meteor.isClient) {
 			//sort picks by creation (newest first),
 			//limit number of displayed entries
             return Picks.find({}, {sort: {createdAt: -1}, limit: MAX_PICKS});
+        }
+    });
+    
+    Template.newPickTemplate.events({
+        "submit .new-pick": function(event) {
+            // This function is called when the new pick form is submitted
+            var title = event.target.title.value;
+			//insert to database
+            Picks.insert({
+                title: title,
+                createdAt: new Date() // current time
+            });
+            // Clear form
+            event.target.title.value = "";
+            
+            window.location = "/picks";
+            
+            // Prevent default form submit
+            return false;
         }
     });
 }
