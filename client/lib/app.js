842d49eb57dc9445151d2e1f23de99015311916f
oshai <ohadshai@gmail.com>
2015-05-25 13:13:48 +0300

    adding mobile support

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,1 +1,10 @@
 angular.module('pick-a-tech',['angular-meteor', 'ui.router']);
+
+function onReady() {
+  angular.bootstrap(document, ['pick-a-tech']);
+}
+
+if (Meteor.isCordova)
+  angular.element(document).on("deviceready", onReady);
+else
+  angular.element(document).ready(onReady);
