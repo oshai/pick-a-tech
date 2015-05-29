0a3de174fd51582098d89bddcad2b1f7ca22680f
oshai <ohadshai@gmail.com>
2015-05-29 14:46:48 +0300

    pick list and add pick

--- client/labels/labelsList.js
+++ client/labels/labelsList.js
@@ -1,13 +1,18 @@
-angular.module("pick-a-tech").controller("LabelsListCtrl", ['$scope', '$meteor',
-  function($scope, $meteor){
+angular.module("pick-a-tech").controller("LabelsListCtrl",
+  function($scope, $meteor, $rootScope){
 
     $scope.labels = $meteor.collection(Labels).subscribe('labels');
+    $scope.newLabel = {};
 
     $scope.remove = function(label){
       $scope.labels.splice( $scope.labels.indexOf(label), 1 );
+    };
+    $scope.createLabel = function(){
+    	$scope.newLabel.owner=$rootScope.currentUser._id;
+    	$scope.labels.save( $scope.newLabel );
     };
 
     $scope.removeAll = function(){
       $scope.labels.remove();
     };
-}]);
+});
--- client/picks/newPick.js
+++ client/picks/newPick.js
@@ -3,11 +3,16 @@ angular.module("pick-a-tech").controller("NewPickCtrl",
 
 	$scope.picks = $meteor.collection(Picks).subscribe('picks');
 	$scope.labels = $meteor.collection(Labels).subscribe('labels');
+	$scope.newPick = {};
+	$scope.newPick.candidates = [];
 	
 	$scope.createNew = function(){
-		$scope.newPick.owner=$rootScope.currentUser._id; 
-		$scope.picks.push($scope.newPick);
-		$location.path('/picks')
+		$scope.newPick.owner = $rootScope.currentUser._id;
+		$scope.newPick.title =  _.map($scope.newPick.candidates, function(label){ return label.name; }).join(" vs ");
+		$scope.newPick.candidates = _.map($scope.newPick.candidates, function(label){ return label._id; });
+		$scope.picks.save($scope.newPick).then(function(){
+			$location.path('/picks');
+		});
 	};
 
 });
