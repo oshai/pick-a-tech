ae7feff86d13c1a6f61f5d3c0b029a9de2b6f0ab
oshai <ohadshai@gmail.com>
2015-05-31 21:25:22 +0300

    removed label details page

--- client/labels/labelDetails.js
+++
@@ -1,6 +0,0 @@
-angular.module("pick-a-tech").controller("LabelDetailsCtrl", ['$scope', '$stateParams', '$meteor',
-  function($scope, $stateParams, $meteor){
-
-    $scope.label = $meteor.object(Labels, $stateParams.labelId).subscribe('labels');;
-
-}]);
