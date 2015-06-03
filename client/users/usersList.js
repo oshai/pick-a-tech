44ee82d12a16cfb5d3671ba628b0fa70de31c8aa
oshai <ohadshai@gmail.com>
2015-06-03 12:21:29 +0300

    added user score

--- client/users/usersList.js
+++ client/users/usersList.js
@@ -1,5 +1,36 @@
 angular.module("pick-a-tech").controller("UsersListCtrl", ['$scope', '$meteor',
-  function($scope, $meteor){
+    function ($scope, $meteor) {
 
-	$scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
-}]);
+        $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
+        $scope.picks = $meteor.collection(Picks).subscribe('picks');
+        $scope.comments = $meteor.collection(Comments).subscribe('comments');
+
+        $scope.name = function (user) {
+            return user.emails[0].address.substring(0, user.emails[0].address.indexOf('@'));
+        }
+        $scope.score = function (user) {
+            //$meteor.call('vote', $scope.pick._id, $rootScope.currentUser._id, key, id).then(function (data) {
+            //    console.log('success vote', data);
+            //}, function (err) {
+            //    console.log('failed', err);
+            //});
+            var result = 0;
+            var id = user._id;
+            $scope.picks.forEach(function(pick){
+                if (pick.owner !== id) {
+                    return;
+                }
+                result += 5 * (pick.vote_up.length - pick.vote_down.length);
+            });
+            $scope.comments.forEach(function(comment){
+                if (comment.owner !== id) {
+                    return;
+                }
+                result += 10 * (comment.vote_up.length - comment.vote_down.length);
+            });
+            if (result < 1) {
+                return 1;
+            }
+            return result;
+        };
+    }]);
--- server/users.js
+++ server/users.js
@@ -1,3 +1,26 @@
 Meteor.publish("users", function () {
-  return Meteor.users.find({}, {fields: {emails: 1, profile: 1}});
+    return Meteor.users.find({}, {fields: {emails: 1, profile: 1}});
+});
+
+Meteor.methods({
+    getScore: function (user) {
+        var result = 0;
+        var id = user._id;
+        Meteor.picks.forEach(function(pick){
+            if (pick.owner !== id) {
+                return;
+            }
+            result += 5 * (pick.vote_up.length - pick.vote_down.length);
+        });
+        Meteor.comments.forEach(function(comment){
+            if (comment.owner !== id) {
+                return;
+            }
+            result += 10 * (comment.vote_up.length - comment.vote_down.length);
+        });
+        if (result < 1) {
+            return 1;
+        }
+        return result;
+    }
 });
