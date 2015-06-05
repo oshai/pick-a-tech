8bf644cae2dc569e7078f53921b0671f1a52c61d
oshai <ohadshai@gmail.com>
2015-06-05 10:52:58 +0300

    added a check for no user

--- client/users/userBadge.ng.js
+++ client/users/userBadge.ng.js
@@ -1,6 +1,8 @@
 angular.module("pick-a-tech").controller("UserBadgeController",
     function ($scope, $meteor, $rootScope) {
-        $scope.userForScore = $meteor.object(Userinfo, {user_id: $rootScope.currentUser._id});
+        if (null !== $rootScope.currentUser) {
+            $scope.userForScore = $meteor.object(Userinfo, {user_id: $rootScope.currentUser._id});
+        }
 
         $scope.userScore = function(){
             return $scope.userForScore.score;
